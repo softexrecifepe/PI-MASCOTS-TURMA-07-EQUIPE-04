1242f57c910f1171a8a90387bad7cb4bd9e2906a
Rodrigo Vilemen <rodrigo.vilewmen@hotmail.com>
2024-10-07 10:27:45 -0300

    remove: home page

--- src/pages/index..tsx
+++ src/pages/index..tsx
@@ -1,11 +1,5 @@
 import { Header } from "@/components/Header";
-import Image from "next/image";
-import HomePage from "./home";
 
 export default function Home() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
+  return <div>Página inicial</div>;
 }
