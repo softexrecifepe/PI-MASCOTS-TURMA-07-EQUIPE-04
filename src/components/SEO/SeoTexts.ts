be4343e803227581cd095a8640ccfd082c7721b1
Rodrigo Vilemen <rodrigo.vilewmen@hotmail.com>
2024-10-09 10:18:15 -0300

    feat: add project

--- src/components/SEO/SeoTexts.ts
+++ src/components/SEO/SeoTexts.ts
@@ -1,35 +1,44 @@
+// SEO para a página inicial
 export const HomePageTextsSEO = {
-  title: "Página inicial",
-  description: "Gerencie sua clinica veterinária de forma simples e eficiente.",
-  keywords: "clinica veterinária, gerenciamento, pets, animais, maskots",
-  author: "Maskots",
-  url: "https://www.seusite.com",
-  image: "https://www.seusite.com/imagem.jpg",
+  title: "Template Next.js - Inicie Seus Projetos com Facilidade",
+  description:
+    "Um template genérico para Next.js que facilita o início de projetos, com funcionalidades prontas e organização de pastas.",
+  keywords:
+    "template Next.js, projetos Next.js, desenvolvimento web, estrutura de pastas, funções utilitárias",
+  author: "Seu Nome ou Projeto",
+  url: "https://seu-template-next.vercel.app/",
+  image: "@/ui/assets/images/default-home.svg",
 };
 
+// SEO para a página de login
 export const LoginPageTextsSEO = {
-  title: "Página inicial",
-  description: "Gerencie sua clinica veterinária de forma simples e eficiente.",
-  keywords: "clinica veterinária, gerenciamento, pets, animais, maskots",
-  author: "Maskots",
-  url: "https://www.seusite.com",
-  image: "https://www.seusite.com/imagem.jpg",
+  title: "Login - Acesse sua Conta",
+  description:
+    "Faça login para acessar a área administrativa do seu projeto Next.js.",
+  keywords: "login, template Next.js, acesso, conta",
+  author: "Seu Nome ou Projeto",
+  url: "https://seu-template-next.vercel.app/login",
+  image: "@/ui/assets/images/default-login.svg",
 };
 
+// SEO para a página de cadastro
 export const CadastroPageTextsSEO = {
-  title: "Página inicial",
-  description: "Gerencie sua clinica veterinária de forma simples e eficiente.",
-  keywords: "clinica veterinária, gerenciamento, pets, animais, maskots",
-  author: "Maskots",
-  url: "https://www.seusite.com",
-  image: "https://www.seusite.com/imagem.jpg",
+  title: "Cadastro - Crie sua Conta",
+  description:
+    "Cadastre-se para ter acesso ao template e suas funcionalidades personalizadas.",
+  keywords: "cadastro, template Next.js, criar conta",
+  author: "Seu Nome ou Projeto",
+  url: "https://seu-template-next.vercel.app/register",
+  image: "@/ui/assets/images/default-cadastro.svg",
 };
 
+// SEO para o dashboard
 export const DashboardPageTextsSEO = {
-  title: "Página inicial",
-  description: "Gerencie sua clinica veterinária de forma simples e eficiente.",
-  keywords: "clinica veterinária, gerenciamento, pets, animais, maskots",
-  author: "Maskots",
-  url: "https://www.seusite.com",
-  image: "https://www.seusite.com/imagem.jpg",
+  title: "Dashboard - Gerencie seu Projeto Next.js",
+  description:
+    "Acesse o dashboard para visualizar dados e gerenciar seu projeto com facilidade.",
+  keywords: "dashboard, template Next.js, gerenciamento de projeto, dados",
+  author: "Seu Nome ou Projeto",
+  url: "https://seu-template-next.vercel.app/dashboard",
+  image: "@/ui/assets/images/default-dashboard.svg",
 };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { HeadLinks } from "@/components/Links";
 import Document, {
   Html,
