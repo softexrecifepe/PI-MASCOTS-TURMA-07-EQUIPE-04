5621a1dd72394324b2c33790a94ecd61604db4aa
Alef <alefadonais5@gmail.com>
2024-10-30 10:47:28 -0300

    feat: implementação da logo no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,13 @@
-import { HeaderContainer } from "@/ui/styles/Components/Header/styles";
 import React from "react";
+import { HeaderContainer, Searchbar} from "@/ui/styles/Components/Header/styles";
+import Image from "next/image";
+import img from "@/ui/assets/images/Logo.svg"
 
 export function Header() {
   return <HeaderContainer>
-          <div>
-              <span>Header</span>
-          </div>
+            <Image alt="" src={img}/>
+            <Searchbar><input type="text" placeholder="" name="search"/></Searchbar> 
+          {/* <Imagemball alt="" src={img}/> */}
+
          </HeaderContainer>
 }
--- src/ui/styles/Components/Header/styles.ts
+++ src/ui/styles/Components/Header/styles.ts
@@ -1,13 +1,22 @@
+import Image from "next/image";
 import styled from "styled-components";
 
 export const HeaderContainer = styled.div`
     border: 2px #442150; 
     background-color: #442150;
-    color: white;
     flex-shrink: 0;
     height: 76px;
+    display: flex;
+    color: white;
 `;
 
-export const div = styled.div`
+export const Searchbar = styled.div`
     justify-content: center;
+
 `;
+
+
+// export const Image = styled(Image)`
+//     margin-left: 2rem;
+    
+// `;
