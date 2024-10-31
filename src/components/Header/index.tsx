50234239198310f767ac090d4fbb4b2e04658c54
alefadonais5 <alefadonais5@gmail.com>
2024-10-31 00:28:11 -0300

    feat: Inseri os icones do header.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,20 @@
 import React from "react";
-import { HeaderContainer, Searchbar, ImageLogo, ImageMenu } from "@/ui/styles/Components/Header/styles";
+import { HeaderContainer, StyledInput, ImageLogo, ImageMenu, Notification, Iconprofile } from "@/ui/styles/Components/Header/styles";
 import Image from "next/image";
 import img from "@/ui/assets/images/Logo 50x50.svg";
 import imgmenu from "@/ui/assets/images/menu.svg"
+import imgnotification from "@/ui/assets/icons/Notoficstion icon.svg"
+import iconprofile from "@/ui/assets/icons/Vector.svg"
 
 export function Header() {
   return (
     <HeaderContainer>
       <ImageMenu  alt="menu" src={imgmenu}/>
       <ImageLogo alt="logo" src={img} />
-      <Searchbar type="text" placeholder="Search..." name="search" />
+      <StyledInput type="text" placeholder="Search..." name="search" />
+      <Notification alt="notification" src={imgnotification}/>
+      <Iconprofile alt="Iconprofile" src={iconprofile}/>
+
     </HeaderContainer>
   );
 }
--- src/ui/styles/Components/Header/styles.ts
+++ src/ui/styles/Components/Header/styles.ts
@@ -5,25 +5,45 @@ export const HeaderContainer = styled.div`
     border: 2px solid #442150; 
     background-color: #442150;
     flex-shrink: 0;
-    height: 76px;
+    width: 100vw; /* Largura igual ao viewport */
+    height: 76px; /* Altura igual ao viewport */
     display: flex;
-    justify-content: space-between; /* Alinhamento horizontal */
+    justify-content: flex-start; /* Alinhamento horizontal */
     align-items: center;           /* Alinhamento vertical */
     padding: 0 2rem;               /* Espaço nas bordas */
 `;
 
-export const Searchbar = styled.div`
-    display: flex;
+export const StyledInput = styled.input`
     border-radius: 25px;
-    border: 1px solid var(--Color-1, #44215D);
-    background: #D9D9D9;
-    padding: 0.5rem 1rem;          /* Espaço interno na barra de pesquisa */
+    border: 1px solid #44215D;
+    background-color: #D9D9D9;
+    padding: 0.5rem 1rem;
+    margin: 0 1.5rem;
+    font-size: 16px;
+    width: 100%;
+    max-width: 300px; /* Adicionada uma largura máxima */
+    outline: none;
+
+    /* Adicionado estilo para o placeholder */
+    &::placeholder {
+        color: #666;
+    }
 `;
 
-export const ImageLogo = styled(Image)`
-     margin-left: 0rem;
-`;
 
 export const ImageMenu = styled(Image)`
-    margin-left: 1rem;
+    margin: 1.75rem 1.75rem 2rem;
+    /* margin-left: 1rem; */
+`;
+export const ImageLogo = styled(Image)`
+     margin-left: 2rem;
+`;
+
+export const Notification = styled(Image)`
+    margin-left: auto;
+    margin-right: 2rem;
+`;
+
+export const Iconprofile = styled(Image)`
+    margin-right: 1.2rem;
 `;
