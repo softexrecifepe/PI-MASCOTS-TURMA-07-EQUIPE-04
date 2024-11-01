f195206860b3bb490ba700527d399bc67ede9cc0
alefadonais5 <alefadonais5@gmail.com>
2024-10-31 21:53:08 -0300

    refactor: Conclusão com componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,23 @@
 import React from "react";
-import { HeaderContainer, StyledInput, ImageLogo, ImageMenu, Notification, Iconprofile } from "@/ui/styles/Components/Header/styles";
+import { HeaderContainer, StyledInputContainer, StyledInput, ImageLogo, ImageMenu, Notification, Iconprofile, IconSearch } from "@/ui/styles/Components/Header/styles";
 import Image from "next/image";
 import img from "@/ui/assets/images/Logo 50x50.svg";
-import imgmenu from "@/ui/assets/images/menu.svg"
-import imgnotification from "@/ui/assets/icons/Notoficstion icon.svg"
-import iconprofile from "@/ui/assets/icons/Vector.svg"
+import imgmenu from "@/ui/assets/images/menu.svg";
+import imgnotification from "@/ui/assets/icons/Notoficstion icon.svg";
+import iconprofile from "@/ui/assets/icons/Vector.svg";
+import iconsearch from "@/ui/assets/icons/Search.svg";
 
 export function Header() {
   return (
     <HeaderContainer>
-      <ImageMenu  alt="menu" src={imgmenu}/>
+      <ImageMenu alt="menu" src={imgmenu} />
       <ImageLogo alt="logo" src={img} />
-      <StyledInput type="text" placeholder="Search..." name="search" />
-      <Notification alt="notification" src={imgnotification}/>
-      <Iconprofile alt="Iconprofile" src={iconprofile}/>
-
+      <StyledInputContainer>
+        <IconSearch alt="search icon" src={iconsearch} />
+        <StyledInput type="text" name="search" />
+      </StyledInputContainer>
+      <Notification alt="notification" src={imgnotification} />
+      <Iconprofile alt="Iconprofile" src={iconprofile} />
     </HeaderContainer>
   );
 }
--- src/ui/styles/Components/Header/styles.ts
+++ src/ui/styles/Components/Header/styles.ts
@@ -2,36 +2,57 @@ import Image from "next/image";
 import styled from "styled-components";
 
 export const HeaderContainer = styled.div`
-    border: 2px solid #442150; 
-    background-color: #442150;
-    flex-shrink: 0;
-    width: 100vw; /* Largura igual ao viewport */
-    height: 76px; /* Altura igual ao viewport */
-    display: flex;
-    justify-content: flex-start; /* Alinhamento horizontal */
-    align-items: center;           /* Alinhamento vertical */
-    padding: 0 2rem;               /* Espaço nas bordas */
+  border: 0.125rem solid #442150;
+  background-color: #442150;
+  flex-shrink: 0;
+  width: 100vw;
+  height: 4.75rem; 
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 0;
+`;
+
+export const StyledInputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: white;
+  border-radius: 1.5625rem; 
+  margin-left: 33rem;
+  margin-right: 32rem;
+  width: 34rem; 
+  padding: 0 0.625rem; 
 `;
 
 export const StyledInput = styled.input`
-    align-items: center;
-    border-radius: 25px;
+  border: none;
+  border-radius: 1.5625rem; 
+  width: 100%;
+  height: 1.875rem; 
+  padding-left: 0.625rem; 
+  outline: none;
 `;
 
+export const IconSearch = styled(Image)`
+  width: 2rem; 
+  height: 2rem; 
+  margin-right: 0.5rem; 
+`;
 
 export const ImageMenu = styled(Image)`
-    margin: 1.75rem 1.75rem 2rem;
-    /* margin-left: 1rem; */
+  margin: 1.75rem 1.75rem 2rem;
 `;
+
 export const ImageLogo = styled(Image)`
-     margin-left: 2rem;
+  margin-left: 1rem;
 `;
 
 export const Notification = styled(Image)`
-    margin-left: auto;
-    margin-right: 2rem;
+  margin-left: auto;
+  margin-right: 2rem;
 `;
 
 export const Iconprofile = styled(Image)`
-    margin-right: 1.2rem;
+  margin-right: 1.2rem;
 `;
+
