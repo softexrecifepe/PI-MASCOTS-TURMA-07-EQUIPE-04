390a9c6b2939f252f2f1c0dcc09a452a61d3985d
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-11-04 07:55:31 -0300

    feat: adiciona itens ao componente Navbar

---
+++ src/components/Navbar/itens.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export const itens = [
+  {
+    id: "1",
+    icon: "🩺",
+    title: "Consultas",
+  },
+  {
+    id: "2",
+    icon: "🏥",
+    title: "Internamentos",
+  },
+  {
+    id: "3",
+    icon: "📦",
+    title: "Estoque",
+  },
+  {
+    id: "4",
+    icon: "📈",
+    title: "Previsão de estoque",
+  },
+  {
+    id: "5",
+    icon: "⬅️",
+    title: "Sair",
+  },
+];
