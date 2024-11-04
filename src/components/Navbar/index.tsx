e7bb9f3036ac7e02e93f4b272de627bc7900c8cb
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-11-04 07:55:55 -0300

    feat: atualiza estilo e estrutura do componente Navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { Container } from "@/ui/styles/Components/Navbar/styles";
+import { Container, Itens } from "@/ui/styles/Components/Navbar/styles";
 import { itens } from "./itens";
 
 export const Navbar: React.FC = () => {
   return (
     <Container>
       {itens.map((item) => (
-        <div key={item.id}>
+        <Itens key={item.id}>
           {item.icon}
           {item.title}
-        </div>
+        </Itens>
       ))}
     </Container>
   );
--- src/ui/styles/Components/Navbar/styles.ts
+++ src/ui/styles/Components/Navbar/styles.ts
@@ -1,8 +1,19 @@
 import styled from "styled-components";
 
 export const Container = styled.nav`
-  color: red;
+  width: 17rem;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.colors.backgrounds.primary};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 5rem;
+  gap: 2rem;
+`;
+
+export const Itens = styled.div`
+  background-color: blue;
+  width: 100%;
   display: flex;
   justify-content: center;
-  align-items: center;
 `;
