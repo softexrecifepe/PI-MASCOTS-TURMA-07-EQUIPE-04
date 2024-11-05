9732197696c9cac5b0485c2e2bdb9f6be38a65b8
Alef Adonais <alefadonais5@gmail.com>
2024-11-05 15:16:40 -0300

    feat: componente Header na página home

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -5,7 +5,7 @@ import { Header } from "../Header";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <Header />
+      
       <main>{children}</main>
       {/* <Footer /> */}
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import SEO from "@/components/SEO";
 import { HomePageTextsSEO } from "@/components/SEO/seoTexts";
 import { Button } from "@/components/Button";
+import { Header } from "@/components/Header";
 
 export default function Home() {
   const seoText = HomePageTextsSEO;
 
   return (
     <div>
+      <Header />
       <SEO
         title={seoText.title}
         description={seoText.description}
