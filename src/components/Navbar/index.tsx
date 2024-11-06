b90835284711f63141de90a9e4d0139ff4921b97
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-11-06 00:00:46 -0300

    fix: adiciona navegação à Navbar e define caminhos para os itens

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Container, Itens } from "@/ui/styles/Components/Navbar/styles";
 import { itens } from "./itens";
+import { useRouter } from "next/router";
 
 interface NavbarProps {
   $isVisible: boolean;
 }
 
 export const Navbar: React.FC<NavbarProps> = ({ $isVisible }) => {
+  const router = useRouter();
+  const handleNavigation = (path: string) => () => router.push(path);
+
   return (
     <Container $isVisible={$isVisible}>
       {itens.map((item) => (
-        <Itens key={item.id}>
+        // <Itens key={item.id} onClick={() => handleNavigation(item.path)}>
+        <Itens key={item.id} onClick={handleNavigation(item.path)}>
           <div>{item.icon}</div>
           <div>{item.title}</div>
         </Itens>
--- src/components/Navbar/itens.tsx
+++ src/components/Navbar/itens.tsx
@@ -1,29 +1,32 @@
-import React from "react";
-
 export const itens = [
   {
     id: "1",
     icon: "🩺",
     title: "Consultas",
+    path: "/consultas",
   },
   {
     id: "2",
     icon: "🏥",
     title: "Internamentos",
+    path: "/consultas",
   },
   {
     id: "3",
     icon: "📦",
     title: "Estoque",
+    path: "/consultas",
   },
   {
     id: "4",
     icon: "📈",
     title: "Previsão de estoque",
+    path: "/consultas",
   },
   {
     id: "5",
     icon: "⬅️",
     title: "Sair",
+    path: "/consultas",
   },
 ];
