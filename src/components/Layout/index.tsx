183a04085ae223d0045a94a237c52a0e84246154
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-11-05 23:00:47 -0300

    fix: adiciona componente SEO ao layout para otimização de metadados

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Footer } from "../Footer";
 import { Header } from "../Header";
 import { usePathname } from "next/navigation";
+import SEO from "../SEO";
+import { DefaultTextsSEO } from "../SEO/seoTexts";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
@@ -9,6 +11,14 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <>
+      <SEO
+        title={DefaultTextsSEO.title}
+        description={DefaultTextsSEO.description}
+        keywords={DefaultTextsSEO.keywords}
+        author={DefaultTextsSEO.author}
+        url={DefaultTextsSEO.url}
+        image={DefaultTextsSEO.image}
+      />
       {ViewLayoutComponents && <Header />}
       <main>{children}</main>
       {/* <Footer /> */}
