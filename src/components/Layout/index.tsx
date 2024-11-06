22c8492fcbabed832cb6903d794dd6028a08a4b4
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-11-05 22:48:59 -0300

    fix: adiciona renderização condicional do Header com base na rota atual

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Footer } from "../Footer";
 import { Header } from "../Header";
+import { usePathname } from "next/navigation";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+  const ViewLayoutComponents = pathname === "/" || pathname === "/dashboard";
+
   return (
     <>
-      
+      {ViewLayoutComponents && <Header />}
       <main>{children}</main>
       {/* <Footer /> */}
     </>
