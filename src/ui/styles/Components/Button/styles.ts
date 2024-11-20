70a5d34b4e9f0f184ecb8f8a4c20e3494213d368
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-11-20 13:13:36 -0300

    feat: rename Button component styles for consistency and clarity

--- src/ui/styles/Components/Button/styles.ts
+++ src/ui/styles/Components/Button/styles.ts
@@ -1,27 +1,26 @@
 import styled from "styled-components";
 
-export const PrimaryButton = styled.button`
-    width: 27.75rem;
-    height: 2.813rem;
-    border: none;
-    background-color: ${({theme}) => theme.colors.backgrounds.primary};
-    border-radius: 0.5rem;
-    color: ${({theme}) => theme.colors.text.secondary};
-    text-align: center;
-    cursor: pointer;
+export const PrimaryButtonStyle = styled.button`
+  width: 27.75rem;
+  height: 2.813rem;
+  border: none;
+  background-color: ${({ theme }) => theme.colors.backgrounds.primary};
+  border-radius: 0.5rem;
+  color: ${({ theme }) => theme.colors.text.secondary};
+  text-align: center;
+  cursor: pointer;
 `;
 
-export const SecondaryButton = styled.button`
-    display: flex;
-    width: 9.4444rem;
-    height: 1.5625rem;
-    border: none;
-    background-color: ${({ theme }) => theme.colors.backgrounds.secondary};
-    border-radius: 0.5rem;
-    color: ${({ theme }) => theme.colors.text.secondary};
-    padding: .4169rem;
-    text-align: center;
-    justify-content: center;
-    cursor: pointer;
+export const SecondaryButtonStyle = styled.button`
+  display: flex;
+  width: 9.4444rem;
+  height: 1.5625rem;
+  border: none;
+  background-color: ${({ theme }) => theme.colors.backgrounds.secondary};
+  border-radius: 0.5rem;
+  color: ${({ theme }) => theme.colors.text.secondary};
+  padding: 0.4169rem;
+  text-align: center;
+  justify-content: center;
+  cursor: pointer;
 `;
-
