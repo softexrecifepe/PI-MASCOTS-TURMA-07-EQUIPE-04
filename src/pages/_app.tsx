3785c4458f5ef3916142769d21d8a11f91eb60f0
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-11-26 01:56:45 -0300

    feat: refactor _app.tsx to use AuthProvider and adjust component structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,19 @@
-import Layout from "@/components/Layout";
-import { HeaderProvider } from "@/contexts/HeaderContext";
+import { AuthProvider } from "@/contexts/AuthContext";
+import { ThemeProvider } from "styled-components";
 import GlobalStyles from "@/ui/styles/GlobalStyles";
 import theme from "@/ui/styles/theme";
-import type { AppProps } from "next/app";
-import { ThemeProvider } from "styled-components";
+import { AppProps } from "next/app";
+import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <HeaderProvider>
+    <AuthProvider>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
         <Layout>
           <Component {...pageProps} />
         </Layout>
-      </HeaderProvider>
-    </ThemeProvider>
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
