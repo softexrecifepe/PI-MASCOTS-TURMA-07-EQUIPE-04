d2fb855a1e916d6b58f6e30fc7153026b4286118
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-11-26 01:02:51 -0300

    feat: replace string icons with SVG components in Navbar

--- src/components/Navbar/itens.tsx
+++ src/components/Navbar/itens.tsx
@@ -1,31 +1,37 @@
+import ForecastIcon from "@/ui/assets/icons/Forecast";
+import HospitalIcon from "@/ui/assets/icons/Hospital";
+import LogoutIcon from "@/ui/assets/icons/Logout ";
+import ServiceIcon from "@/ui/assets/icons/Service";
+import StockIcon from "@/ui/assets/icons/Stock";
+
 export const itens = [
   {
     id: "1",
-    icon: "🩺",
+    icon: <ServiceIcon />,
     title: "Consultas",
     path: "/consultas",
   },
   {
     id: "2",
-    icon: "🏥",
+    icon: <HospitalIcon />,
     title: "Internamentos",
     path: "/consultas",
   },
   {
     id: "3",
-    icon: "📦",
+    icon: <StockIcon />,
     title: "Estoque",
     path: "/consultas",
   },
   {
     id: "4",
-    icon: "📈",
+    icon: <ForecastIcon />,
     title: "Previsão de estoque",
     path: "/consultas",
   },
   {
     id: "5",
-    icon: "⬅️",
+    icon: <LogoutIcon />,
     title: "Sair",
     path: "/consultas",
   },
