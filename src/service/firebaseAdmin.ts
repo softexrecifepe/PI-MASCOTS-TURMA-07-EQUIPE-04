7ac25c4cbf279eac6a8c4466dc4a37160906b791
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-11-27 03:36:50 -0300

    feat: add Firebase Admin service for token verification

---
+++ src/service/firebaseAdmin.ts
@@ -0,0 +1,20 @@
+// service/firebaseAdmin.ts
+import admin from "firebase-admin";
+
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT_KEY || "{}"
+);
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+export const verifyIdToken = async (token: string) => {
+  try {
+    return await admin.auth().verifyIdToken(token);
+  } catch (err) {
+    throw new Error("Token inválido");
+  }
+};
