368ff44c597400ca8f976807a0e4010b509dc42d
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-11-27 07:25:40 -0300

    feat: add consultation creation form with toggle functionality on Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { GetServerSideProps } from "next";
 import nookies from "nookies";
 import SEO from "@/components/SEO";
@@ -15,10 +15,20 @@ import {
 import { SecundaryButton } from "@/components/Elements/Buttons";
 import { useRouter } from "next/router";
 import { useHeader } from "@/contexts/HeaderContext";
+import { Vetconsultation } from "@/components/AddConsulta";
 
 export default function Home() {
   const router = useRouter();
   const navBarIsVisible = useHeader();
+  const [isFormVisible, setIsFormVisible] = useState<boolean>(false); // Estado para controlar a exibição do formulário de criação de consulta
+
+  const handleCreateConsultationClick = () => {
+    setIsFormVisible(true); // Exibe o formulário de criação de consulta
+  };
+
+  const handleCancelForm = () => {
+    setIsFormVisible(false); // Fecha o formulário sem salvar
+  };
 
   return (
     <Container>
@@ -31,31 +41,40 @@ export default function Home() {
         image={HomePageTextsSEO.image}
       />
 
-      <ButtonContainer navBarVisible={navBarIsVisible.isNavbarVisible}>
-        <Title>Consultas</Title>
+      {/* Se o formulário não estiver visível, mostra os botões e cards */}
+      {!isFormVisible ? (
+        <ButtonContainer navBarVisible={navBarIsVisible.isNavbarVisible}>
+          <Title>Consultas</Title>
 
-        <div>
-          <SecundaryButton onClick={() => router.push("historico")}>
-            Histórico
-          </SecundaryButton>
-          <SecundaryButton onClick={() => alert("Botão Secundário Clicado!")}>
-            Criar Consulta
-          </SecundaryButton>
-        </div>
-      </ButtonContainer>
+          <div>
+            <SecundaryButton onClick={() => router.push("historico")}>
+              Histórico
+            </SecundaryButton>
+            <SecundaryButton onClick={handleCreateConsultationClick}>
+              Criar Consulta
+            </SecundaryButton>
+          </div>
+        </ButtonContainer>
+      ) : (
+        // Se o formulário estiver visível, renderiza o componente de criação de consulta
+        <Vetconsultation onCancel={handleCancelForm} />
+      )}
 
-      <GroupCards>
-        {cardItems.map((card) => (
-          <Card
-            key={card.id}
-            image={card.imageUrl || imgExample.src}
-            animalName={card.animalName}
-            date={card.date}
-            hour={card.hour}
-            description={card.description}
-          />
-        ))}
-      </GroupCards>
+      {/* Renderiza os cards se o formulário não estiver visível */}
+      {!isFormVisible && (
+        <GroupCards>
+          {cardItems.map((card) => (
+            <Card
+              key={card.id}
+              image={card.imageUrl || imgExample.src}
+              animalName={card.animalName}
+              date={card.date}
+              hour={card.hour}
+              description={card.description}
+            />
+          ))}
+        </GroupCards>
+      )}
     </Container>
   );
 }
